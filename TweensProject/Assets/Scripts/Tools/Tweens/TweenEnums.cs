089ec43f7b00632b517dfb67a1bfd5b8219a5e8a
Auguste Paccapelo <paccapelo.auguste@gmail.com>
2025-10-31 08:46:17 +0100

    [ADD] Key words for some recurrent tweens properties

--- TweensProject/Assets/Scripts/Tools/Tweens/TweenEnums.cs
+++ TweensProject/Assets/Scripts/Tools/Tweens/TweenEnums.cs
@@ -1,5 +1,5 @@
 // Author : Auguste Paccapelo
-
+using UnityEngine;
 public enum TweenEase
 {
     In, Out, InOut, OutIn, Custom
@@ -9,3 +9,19 @@ public enum TweenType
 {
     Linear, Sine, Cubic, Quint, Circ, Elastic, Quad, Quart, Expo, Back, Bounce, Custom
 }
+
+public static class TweenTarget
+{
+    public static class Transform
+    {
+        public const string POSITION = "position";
+        public const string LOCAL_SCALE = "localScale";
+        public const string ROTATION_QUATERNION = "rotation";
+        public const string ROTATION_EULER_ANGLE = "eulerAngles";
+    }
+
+    public static class Renderer
+    {
+        public const string COLOR = "color";
+    }
+} 
